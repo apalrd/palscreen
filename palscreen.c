a891c3741796d7b00e90e8b70d50f1e070cba0a8
Andrew <apalrd@gmail.com>
2014-12-07 00:45:01 -0500

    Added sleep back

--- palscreen.c
+++ palscreen.c
@@ -7,16 +7,8 @@ PalScreen user interface for PiTFT
 #include <sys/types.h>
 #include <unistd.h>
 #include <stdio.h>
-#include <signal.h>
 #include "palscreen.h"
 #include "gfx.h"
-
-
-
-void sign_handler(void)
-{
-    printf("Got a signal!!!");
-}
 
 //Main
 int main()
@@ -26,9 +18,6 @@ int main()
     GFXInit();
     
     printf("GFX init complete\n");
-    
-    printf("Setting signal handler\n");
-    signal(SIGBUS, sign_handler);
     
     //Change the color continuously
     while(1)
@@ -43,46 +32,37 @@ int main()
         GFXSwapBuffer();
         
         //Wait for at least a second
-        //sleep(1);
-        
-        //Fill the screen another color
-        GFXFillScreen(0x0000);
-        
-        //Swap the buffers again
-        GFXSwapBuffer();
-        
-        //Wait at least a second
-        //sleep(1);
+        sleep(1);
         
         //Blue
         GFXFillScreen(GFXPixelColor(0x00,0x00,0xFF));
         GFXSwapBuffer();
-        //sleep(1);
+        sleep(1);
         
         //Red
         GFXFillScreen(GFXPixelColor(0xFF,0x00,0x00));
         GFXSwapBuffer();
-        //sleep(1);
+        sleep(1);
         
         //Green
         GFXFillScreen(GFXPixelColor(0x00,0xFF,0x00));
         GFXSwapBuffer();
-        //sleep(1);
+        sleep(1);
         
         //Orange
         GFXFillScreen(GFXPixelColor(0xFF,0x80,0x00));
         GFXSwapBuffer();
-        //sleep(1);
+        sleep(1);
         
         //Magenta
         GFXFillScreen(GFXPixelColor(0xFF,0x00,0xFF));
         GFXSwapBuffer();
-        //sleep(1);
+        sleep(1);
         
         //Yellow
         GFXFillScreen(GFXPixelColor(0xFF,0xFF,0x00));
         GFXSwapBuffer();
-        //sleep(1);
+        sleep(1);
     }
 
 }
